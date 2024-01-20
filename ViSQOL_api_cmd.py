2b4fa6cc7f10e828f2708b1f0f41587a14ff0f88
ted stinson <30837044+luuumity@users.noreply.github.com>
2024-01-20 11:10:13 +0800

    Update ViSQOL_api_cmd.py

--- ViSQOL_api_cmd.py
+++ ViSQOL_api_cmd.py
@@ -1,7 +1,6 @@
 # 此脚本包含批量重采样至16kHz的功能。
 # 最终通过命令行（子进程）方式实现。
-# 但应该需要pip install .，使用setup.py之后才能导入这些模块。
-# 但如果只用到一个visqol_lib_py.so文件的话，我也许可以直接copy这一个文件，因为Colab上安装环境有些问题。
+# 无需导入了，因为本质上就是代替命令行操作，只需要搞清楚模型在哪即可。
 import torchaudio
 import os
 #import numpy
@@ -10,7 +9,8 @@ import subprocess
 import re
 
 # 需要pip install .，使用setup.py之后才能导入这些模块。
-from visqol import visqol_lib_py
+# 无需导入了，因为本质上就是代替命令行操作，只需要搞清楚模型在哪即可。
+# from visqol import visqol_lib_py
 # from visqol.pb2 import visqol_config_pb2
 # from visqol.pb2 import similarity_result_pb2
 
@@ -40,9 +40,9 @@ def eval(i, operate_path):
         visqol_dir = "/Users/sunjiayi/Downloads/visqol/"
         visqol_path = visqol_dir + "bazel-bin/visqol"
 		# 确定所用模型的绝对路径
-        svr_model_path = "lattice_tcditugenmeetpackhref_ls2_nl60_lr12_bs2048_learn.005_ep2400_train1_7_raw.tflite"
-        model_path = os.path.join(
-            os.path.dirname(visqol_lib_py.__file__), "model", svr_model_path)
+        model_path = "./lattice_tcditugenmeetpackhref_ls2_nl60_lr12_bs2048_learn.005_ep2400_train1_7_raw.tflite"
+        # model_path = os.path.join(
+            # os.path.dirname(visqol_lib_py.__file__), "model", svr_model_path)
         result = subprocess.run([visqol_path,"--use_speech_mode","--reference_file",reference_file,"--degraded_file",degraded_file,"--similarity_to_quality_model",model_path], 
                                 capture_output=True, text=True)
 		# 使用正则表达式剥去命令行的输出，获得浮点数。
